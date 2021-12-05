ba8c8635981e4895f213a8f26a0e528a47a59629
Juan José Longoria <juan.longoria.lopez@accenture.com>
2021-12-05 23:37:37 +0100

    Transform types/lightningExperienceChannel.js into ts file

--- src/definitions/lightningMessageChannel.ts
+++ src/definitions/lightningMessageChannel.ts
@@ -1,6 +1,6 @@
-const { StringXMLField, ArrayXMLField, BooleanXMLField } = require('@aurahelper/core').Types;
+import { StringXMLField, BooleanXMLField, ArrayXMLField } from '@aurahelper/core';
 
-module.exports = {
+export const LightningMessageChannel = {
     description: new StringXMLField('description', 'Description')
         .setMinApi(47)
         .setEditable(),
