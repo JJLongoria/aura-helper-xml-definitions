987269ef16e339094466a95cefc0c5435461856a
Juan José Longoria <juan.longoria.lopez@accenture.com>
2021-12-05 19:52:21 +0100

    Transform types/embeddedServiceMenuSettings.js into ts file

--- src/definitions/embeddedServiceMenuSettings.ts
+++ src/definitions/embeddedServiceMenuSettings.ts
@@ -1,7 +1,6 @@
-const { StringXMLField, ArrayXMLField, IntegerXMLField, EnumXMLField, BooleanXMLField, XMLDependencyField } = require('@aurahelper/core').Types;
-const { MetadataTypes, DataValues } = require('@aurahelper/core').Values;
+import { StringXMLField, BooleanXMLField, ArrayXMLField, XMLDependencyField, DataValues, MetadataTypes, EnumXMLField, IntegerXMLField } from '@aurahelper/core';
 
-module.exports = {
+export const EmbeddedServiceMenuSettings = {
     branding: new StringXMLField('branding', 'Branding')
         .setMinApi(47)
         .setEditable(),
