c6f6dc9b643ccfa45e36f4eb6e52693301fb39c3
Juan José Longoria <juan.longoria.lopez@accenture.com>
2021-12-05 22:56:09 +0100

    Create new forecastingTypeSource.ts file to support API 53.0

--- src/definitions/forecastingType.ts
+++ src/definitions/forecastingType.ts
@@ -1,4 +1,4 @@
-import { StringXMLField, BooleanXMLField, ArrayXMLField, XMLDependencyField, DataValues, MetadataTypes, EnumXMLField, IntegerXMLField, DoubleXMLField, ObjectXMLField } from '@aurahelper/core';
+import { StringXMLField, BooleanXMLField, EnumXMLField } from '@aurahelper/core';
 
 export const ForecastingType = {
     active: new BooleanXMLField('active', 'Active')
