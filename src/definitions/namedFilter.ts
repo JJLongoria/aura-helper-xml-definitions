4420779ae67cf49157a9773f65b7159f3d46d3e9
Juan José Longoria <juan.longoria.lopez@accenture.com>
2021-12-06 00:01:13 +0100

    Transform types/namedFilter.js into ts file

--- src/definitions/namedFilter.ts
+++ src/definitions/namedFilter.ts
@@ -1,7 +1,6 @@
-const { StringXMLField, ArrayXMLField, EnumXMLField, BooleanXMLField } = require('@aurahelper/core').Types;
-const { MetadataTypes } = require('@aurahelper/core').Values;
+import { StringXMLField, BooleanXMLField, ArrayXMLField, MetadataTypes, EnumXMLField } from '@aurahelper/core';
 
-module.exports = {
+export const NamedFilter = {
     active: new BooleanXMLField('active', 'Active')
         .setMinApi(17)
         .setEditable()
