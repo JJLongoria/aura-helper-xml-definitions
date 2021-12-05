b8f7b29b921de4dac82105b29ab0bb0c2bea2ec0
Juan José Longoria <juan.longoria.lopez@accenture.com>
2021-12-06 00:02:39 +0100

    Transform types/networkBranding.js into ts file

--- src/definitions/networkBranding.ts
+++ src/definitions/networkBranding.ts
@@ -1,6 +1,6 @@
-const { StringXMLField } = require('@aurahelper/core').Types;
+import { StringXMLField } from '@aurahelper/core';
 
-module.exports = {
+export const NetworkBranding ={
     loginFooterText: new StringXMLField('loginFooterText', 'Login Footer Text')
         .setMinApi(41)
         .setEditable(),
