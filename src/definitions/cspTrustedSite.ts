3492055b0070a5b5bf58e3d03253761d34af3898
Juan José Longoria <juan.longoria.lopez@accenture.com>
2021-12-05 15:14:42 +0100

    Transform types/cspTrustedSite.js into ts file

--- src/definitions/cspTrustedSite.ts
+++ src/definitions/cspTrustedSite.ts
@@ -1,6 +1,6 @@
-const { StringXMLField, EnumXMLField, BooleanXMLField } = require('@aurahelper/core').Types;
+import { StringXMLField, BooleanXMLField, EnumXMLField } from '@aurahelper/core';
 
-module.exports = {
+export const CspTrustedSite = {
     context: new EnumXMLField('context', 'Context')
         .setMinApi(44)
         .setEditable()
