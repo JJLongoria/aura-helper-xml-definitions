b730c8b86c25e35b098c7ab53a538fa635e02dc6
Juan José Longoria <juanjoselongoria@gmail.com>
2021-12-06 14:37:17 +0100

    Create new outboundNetworkConnection.ts file to support API 53.0

---
+++ src/definitions/outboundNetworkConnection.ts
@@ -0,0 +1,51 @@
+import { StringXMLField, BooleanXMLField, ArrayXMLField, XMLDependencyField, DataValues, MetadataTypes, EnumXMLField, IntegerXMLField, DoubleXMLField, ObjectXMLField } from '@aurahelper/core';
+
+export const OutboundNetworkConnection = {
+    connectionType: new EnumXMLField('connectionType', 'Connection Type')
+        .setMinApi(49)
+        .setEditable()
+        .setRequired()
+        .addEnumValue('AWS Private Link', 'AwsPrivateLink'),
+    description: new StringXMLField('description', 'Description')
+        .setMinApi(49)
+        .setEditable()
+        .setMaxLength(255),
+    isActive: new BooleanXMLField('isActive', 'Is Active')
+        .setMinApi(49)
+        .setEditable()
+        .setRequired()
+        .setDefaultValue(false),
+    label: new StringXMLField('label', 'Label')
+        .setMinApi(49)
+        .setEditable()
+        .setRequired(),
+    outboundNetworkConnProperties: new ArrayXMLField('outboundNetworkConnProperties', 'Outbound Network Conn Properties')
+        .setMinApi(49)
+        .setEditable()
+        .setRequired()
+        .setFieldKey('propertyName')
+        .addField('propertyName', new EnumXMLField('propertyName', 'Property Name')
+            .setEditable()
+            .setRequired()
+            .addEnumValue('AWS Virtual Private Cloud Endpoint Id', 'AwsVpcEndpointId')
+            .addEnumValue('Region', 'Region')
+            .addEnumValue('Source Ip Ranges', 'SourceIpRanges')
+        )
+        .addField('propertyValue', new StringXMLField('propertyValue', 'Property Value')
+            .setEditable()
+            .setRequired()
+            .setJSON()
+        ),
+    status: new EnumXMLField('status', 'Status')
+        .setMinApi(49)
+        .setEditable()
+        .setRequired()
+        .addEnumValue('Unprovisioned', 'Unprovisioned')
+        .addEnumValue('Allocating', 'Allocating')
+        .addEnumValue('Pending Acceptance', 'PendingAcceptance')
+        .addEnumValue('Pending Activation', 'PendingActivation')
+        .addEnumValue('Rejected Remotely', 'RejectedRemotely')
+        .addEnumValue('Deleted Remotely', 'DeletedRemotely')
+        .addEnumValue('Teardown In Progress', 'TeardownInProgress')
+        .addEnumValue('Ready', 'Ready')
+};
