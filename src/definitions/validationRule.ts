aa34bacf80629e19b0f35b854d729a6d06660d44
Juan José Longoria <juanjoselongoria@gmail.com>
2021-12-06 19:28:02 +0100

    Transform types/validationRule.js into ts file

--- src/definitions/validationRule.ts
+++ src/definitions/validationRule.ts
@@ -1,7 +1,6 @@
-const { StringXMLField, BooleanXMLField } = require('@aurahelper/core').Types;
-const { MetadataTypes } = require('@aurahelper/core').Values;
+import { StringXMLField, BooleanXMLField, MetadataTypes } from '@aurahelper/core';
 
-module.exports = {
+export const ValidationRule = {
     active: new BooleanXMLField('active', 'Active')
         .setMinApi(12)
         .setEditable()
