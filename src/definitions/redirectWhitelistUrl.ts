a4f69ed8e2d50dc893a2f2048069912439fb1b46
Juan José Longoria <juanjoselongoria@gmail.com>
2021-12-06 18:22:49 +0100

    Transform types/redirectWhitelistUrl.js into ts file

--- src/definitions/redirectWhitelistUrl.ts
+++ src/definitions/redirectWhitelistUrl.ts
@@ -1,6 +1,6 @@
-const { StringXMLField } = require('@aurahelper/core').Types;
+import { StringXMLField } from '@aurahelper/core';
 
-module.exports = {
+export const RedirectWhitelistUrl = {
     Url: new StringXMLField('Url', 'URL')
         .setMinApi(48)
         .setEditable()
