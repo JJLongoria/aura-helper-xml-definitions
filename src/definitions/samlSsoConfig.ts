9402fbb4406277c8ce925340d1278acdf84b56fb
Juan José Longoria <juanjoselongoria@gmail.com>
2021-12-06 18:39:57 +0100

    Transform types/samlSsoConfig.js into ts file

--- src/definitions/samlSsoConfig.ts
+++ src/definitions/samlSsoConfig.ts
@@ -1,7 +1,6 @@
-const { StringXMLField, EnumXMLField, BooleanXMLField } = require('@aurahelper/core').Types;
-const { MetadataTypes } = require('@aurahelper/core').Values;
+import { StringXMLField, BooleanXMLField, MetadataTypes, EnumXMLField } from '@aurahelper/core';
 
-module.exports = {
+export const SamlSsoConfig = {
     attributeNameIdFormat: new StringXMLField('attributeNameIdFormat', 'Attribute Name Id Format')
         .setMinApi(28)
         .setEditable(),
