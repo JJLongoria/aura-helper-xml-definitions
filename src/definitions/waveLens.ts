c4dac872f4c557d466a72135a64b4d98dde79397
Juan José Longoria <juan.longoria.lopez@accenture.com>
2021-12-06 19:30:22 +0100

    Transform types/waveLens.js into ts file

--- src/definitions/waveLens.ts
+++ src/definitions/waveLens.ts
@@ -1,7 +1,6 @@
-const { StringXMLField, EnumXMLField } = require('@aurahelper/core').Types;
-const { MetadataTypes } = require('@aurahelper/core').Values;
+import { StringXMLField, MetadataTypes, EnumXMLField } from '@aurahelper/core';
 
-module.exports = {
+export const WaveLens = {
     application: new StringXMLField('application', 'Application')
         .setMinApi(37)
         .setEditable(),
